f6db7e42afd5ac24b535dff027a09dc38974d347
Fernando Jorge <79945109+nandowastaken@users.noreply.github.com>
2022-03-01 21:22:59 -0300

    .advice-container changed
    .advice-container no longer having the background purple, instead, I put that to .the advice-text

--- script-advice.js
+++ script-advice.js
@@ -2,7 +2,7 @@ function getAdvice() {
     const text_advice = document.getElementById("advice-paragraph");
     w3.getHttpObject("https://api.adviceslip.com/advice", function(data) {
         advice = data.slip.advice;
-        text_advice.innerHTML = advice;
+        text_advice.innerHTML = '"' + advice + '"';
     });
 }
 
